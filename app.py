d7a3634dd9a8e9e1396cc14cedc1190064137202
OusamaBahri <bahri.oussama@rocketmail.com>
2022-12-20 10:46:15 +0100

    update

--- app.py
+++ app.py
@@ -3,22 +3,24 @@ import tensorflow as tf
 import numpy as np
 img_height = 48
 img_width = 48
-picture = st.camera_input("Take a picture")
+img_file_buffer = st.camera_input("Take a picture")
 model = tf.keras.models.load_model("EmotionDetecttor2")
 class_names = ['angry', 'disgusted', 'fearful', 'happy', 'neutral', 'sad', 'surprised']
-if picture:
-    st.image(picture)
+if img_file_buffer:
+    st.image(img_file_buffer)
+    bytes_data = img_file_buffer.getvalue()
+    img_tensor = tf.io.decode_image(bytes_data, channels=3)
 
-img = tf.keras.utils.load_img(
-    picture, target_size=(img_height, img_width)
-)
-img_array = tf.keras.utils.img_to_array(img)
-img_array = tf.expand_dims(img_array, 0) # Create a batch
+#img = tf.keras.utils.load_img( picture, target_size=(img_height, img_width))
+# To read image file buffer as a 3D uint8 tensor with TensorFlow:
+    
+#img_array = tf.keras.utils.img_to_array(img)
+#img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-predictions = model.predict(img_array)
-score = tf.nn.softmax(predictions[0])
+    predictions = model.predict(img_tensor)
+    score = tf.nn.softmax(predictions[0])
 
-print(
-    "This image most likely belongs to {} with a {:.2f} percent confidence."
-    .format(class_names[np.argmax(score)], 100 * np.max(score))
-)
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
