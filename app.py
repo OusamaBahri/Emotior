791324642d18bad864498d7081dd539344931ccf
OusamaBahri <bahri.oussama@rocketmail.com>
2022-12-20 18:32:59 +0100

    update

--- app.py
+++ app.py
@@ -40,13 +40,6 @@ if select =="Use your Webcam":
         img = img.convert('RGB')
         img_array = tf.keras.utils.img_to_array(img.resize((48,48)))
         img_array = tf.expand_dims(img_array, 0)
-        st.write(img_array.shape)
-    #img = tf.keras.utils.load_img( picture, target_size=(img_height, img_width))
-    # To read image file buffer as a 3D uint8 tensor with TensorFlow:
-        
-    #img_array = tf.keras.utils.img_to_array(img)
-    #img_array = tf.expand_dims(img_array, 0) # Create a batch
-
         predictions = model.predict(img_array)
         score = tf.nn.softmax(predictions[0])
 
